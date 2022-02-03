6dbfd0ae12416ca34653ccbe69e3962e0bb50bd4
Robert Heath <robert.b.heathii@gmail.com>
2022-02-03 12:19:40 -0600

    Add shells for tests for later iterations

---
+++ spec/requests/annual_statistics_requests_spec.rb
@@ -0,0 +1,9 @@
+equire 'rails_helper'
+
+describe 'annual statistics requests' do
+  describe 'happy path' do
+    it '' do
+
+    end
+  end
+end
---
+++ spec/requests/available_years_requests_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'available years requests' do
+  describe 'happy path' do
+    it 'can return all available years' do
+
+    end
+  end
+end
