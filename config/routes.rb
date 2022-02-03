3b3051494a71b534e6abbf6e87954baae58a1bb2
Robert Heath <robert.b.heathii@gmail.com>
2022-02-03 12:09:59 -0600

    Add error in route handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get '/available-airport-codes', to: 'airport_codes#index'
+  get '/available-years', to: 'years#index'
+  get '/annual-statistics/:year', to: 'years#show'
+  get '/available-quantities', to: 'quantites#index'
+
+  match '*unmatched_route', :to => 'application#raise_not_found!', :via => :all
 end
