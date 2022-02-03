3e1104085efcd66d0b67490b88fed0714ba6aa05
Robert Heath <robert.b.heathii@gmail.com>
2022-02-03 10:10:54 -0600

    Add files to rails_helper/ finish setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,14 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'simplecov'
+SimpleCov.start
+SimpleCov.add_filter %W[
+  app/helpers
+  app/controllers/application_controller
+  config
+  spec
+]
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -61,4 +69,12 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include FactoryBot::Syntax::Methods
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 end
